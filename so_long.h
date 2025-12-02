04633a1945c0abd8dfc9074b5a5d7b4fa13a2243
Buğra Öksüz <bugraoksuz6@gmail.com>
2025-12-02 21:40:19 +0300

    map.bar and so_long.h header

---
+++ so_long.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: boksuz <boksuz@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 21:27:14 by boksuz            #+#    #+#             */
+/*   Updated: 2025/12/02 21:27:14 by boksuz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <unistd.h>
+
+typedef	struct s_myGame
+{
+	void 	*mix;
+	void 	*win;
+	char 	**map;			// haritayı tutacak olan 2D array
+	// diğer görseller - (zemin coin çıkış)
+	void 	*img_wall;		// duvar görseli
+	void 	*img_player;	// oyuncu görseli
+	int 	map_width;
+	int 	map_height;
+	int 	player_x;
+	int 	player_y;
+	int 	collectibles;	// toplanacak coin sayısı
+	int 	moves;			// haraket sayısı
+}t_game;
+
+
+#endif
